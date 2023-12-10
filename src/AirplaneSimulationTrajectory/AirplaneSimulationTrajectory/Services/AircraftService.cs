db11e9a1ef4372e20333dc208f52abe8066d7eaf
Ledrunning <ledrunner@hotmail.com>
2023-12-10 17:16:55 +0100

    Added AircraftService.cs

---
+++ src/AirplaneSimulationTrajectory/AirplaneSimulationTrajectory/Services/AircraftService.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Media.Media3D;
+
+namespace AirplaneSimulationTrajectory.Services
+{
+    internal class AircraftService
+    {
+        private readonly double EarthRadius = 1; //Earth radius
+        private readonly double PlaneScale = 1; //plane scale
+        private double deltaTime = 0.001; //delta time
+        private double HeightOverGround = 0.02;
+        private double MinDistance = 0.01f; //current plane pos
+        private Vector3D PointA; //from point
+        private Vector3D PointB; //to point
+        public Vector3D PlanePosition { get; set; } //current plane pos
+
+        public void SetPlanePath(Vector3D from, Vector3D to)
+        {
+            PointA = Normalized(from) * EarthRadius;
+            PointB = Normalized(to) * EarthRadius;
+            PlanePosition = PointA;
+        }
+
+        private static Vector3D Normalized(Vector3D vector)
+        {
+            return vector / vector.Length;
+        }
+
+        public Transform3D GetPlaneTransform(Vector3D forward, Vector3D up, Vector3D pos)
+        {
+            //look at
+            var v1 = forward;
+            var v3 = up;
+            var v2 = Vector3D.CrossProduct(v1, v3);
+
+            var matrix3D = new Matrix3D(
+                v1.X, v1.Y, v1.Z, 0,
+                v2.X, v2.Y, v2.Z, 0,
+                v3.X, v3.Y, v3.Z, 0,
+                0, 0, 0, 1);
+
+            //scale
+            matrix3D.Scale(new Vector3D(PlaneScale, PlaneScale, PlaneScale));
+
+            //pos
+            matrix3D.Translate(pos);
+
+            return new MatrixTransform3D(matrix3D);
+        }
+    }
+}
--- src/AirplaneSimulationTrajectory/AirplaneSimulationTrajectory/ViewModel/MainViewModel.cs
+++ src/AirplaneSimulationTrajectory/AirplaneSimulationTrajectory/ViewModel/MainViewModel.cs
@@ -1,19 +1,46 @@
-﻿using System.Windows;
+﻿using System;
+using System.Timers;
+using System.Windows;
 using System.Windows.Media.Media3D;
 using AirplaneSimulationTrajectory.View;
+using HelixToolkit.Wpf;
 
 namespace AirplaneSimulationTrajectory.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : BaseViewModel
     {
-        public static readonly DependencyProperty CloudsProperty = DependencyProperty.Register(
-            "Clouds", typeof(Material), typeof(MainWindow), new UIPropertyMetadata(null));
+        private Timer timer;
 
-        /// <summary>
-        /// The sunlight direction property.
-        /// </summary>
-        public static readonly DependencyProperty SunlightDirectionProperty =
-            DependencyProperty.Register(
-                "SunlightDirection", typeof(Vector3D), typeof(MainWindow), new UIPropertyMetadata(new Vector3D()));
+        public MainViewModel()
+        {
+            Clouds = MaterialHelper.CreateImageMaterial("pack://application:,,,/Images/clouds.jpg", 0.5);
+            var now = DateTime.UtcNow;
+            var juneSolstice = new DateTime(now.Year, 6, 22);
+        }
+
+        private Material _clouds;
+
+        private Vector3D _sunlightDirection;
+
+        public Material Clouds
+        {
+            get => _clouds;
+            set
+            {
+                _clouds = value;
+                SetField(ref _clouds, value, nameof(Clouds));
+            }
+        }
+
+        public Vector3D SunlightDirection
+        {
+            get => _sunlightDirection;
+
+            set
+            {
+                _sunlightDirection = value;
+                SetField(ref _sunlightDirection, value, nameof(SunlightDirection));
+            }
+        }
     }
 }
