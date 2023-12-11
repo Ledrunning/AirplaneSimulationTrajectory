52029d3ec2a207020835e269968d97ed30d115fd
Ledrunning <ledrunner@hotmail.com>
2023-12-11 22:27:05 +0100

    Added path and movement calculation

--- src/AirplaneSimulationTrajectory/AirplaneSimulationTrajectory/Services/AircraftService.cs
+++ src/AirplaneSimulationTrajectory/AirplaneSimulationTrajectory/Services/AircraftService.cs
@@ -1,28 +1,52 @@
-﻿using System.Windows.Media.Media3D;
+﻿using System;
+using System.Windows.Media.Media3D;
 
 namespace AirplaneSimulationTrajectory.Services
 {
     internal class AircraftService
     {
-        private readonly double EarthRadius = 1; //Earth radius
-        private readonly double PlaneScale = 1; //plane scale
-        private double deltaTime = 0.001; //delta time
-        private double HeightOverGround = 0.02;
-        private double MinDistance = 0.01f; //current plane pos
+        private readonly double EarthRadius = 1;
+        private readonly double PlaneScale = 1;
+        private readonly double deltaTime = 0.001;
+        private readonly double HeightOverGround = 0.02;
+        private readonly double MinDistance = 0.01f; //current plane pos
         private Vector3D PointA; //from point
         private Vector3D PointB; //to point
-        public Vector3D PlanePosition { get; set; } //current plane pos
+        public Vector3D AircraftPosition { get; set; } //current plane pos
 
         public void SetPlanePath(Vector3D from, Vector3D to)
         {
             PointA = Normalized(from) * EarthRadius;
             PointB = Normalized(to) * EarthRadius;
-            PlanePosition = PointA;
+            AircraftPosition = PointA;
         }
 
         private static Vector3D Normalized(Vector3D vector)
         {
             return vector / vector.Length;
+        }
+
+        public (Transform3D planeTransform, Vector3D secondPosition) UpdateAircraftPosition()
+        {
+            //aircraft are arrived in point B
+            if ((PointB - AircraftPosition).Length < MinDistance)
+            {
+                return default;
+            }
+
+            //calculate orientation and new position of an aircraft
+            var firstPosition = AircraftPosition + Normalized(PointB - AircraftPosition) * deltaTime;
+            var secondPosition = EarthRadius * Normalized(firstPosition);
+            var aircraftDirections = secondPosition - AircraftPosition;
+
+            //plane forward and up direction
+            var forward = Normalized(aircraftDirections);
+            var up = Normalized(firstPosition);
+
+            //apply transform
+            var planeTransform = GetPlaneTransform(forward, up, firstPosition * (1 + HeightOverGround));
+
+            return (planeTransform, secondPosition);
         }
 
         public Transform3D GetPlaneTransform(Vector3D forward, Vector3D up, Vector3D pos)
@@ -45,6 +69,15 @@ namespace AirplaneSimulationTrajectory.Services
             matrix3D.Translate(pos);
 
             return new MatrixTransform3D(matrix3D);
+        }
+
+        public Vector3D CalculateMove(DateTime now, DateTime juneSolstice)
+        {
+            // todo: check calculation - this is probably not correct
+            var declination = 23.45 * Math.Cos((now.DayOfYear - juneSolstice.DayOfYear) / 365.25 * 2 * Math.PI);
+            var phi = -now.Hour / 24.0 * Math.PI * 2;
+            var theta = declination / 180 * Math.PI;
+            return -new Vector3D(Math.Cos(phi) * Math.Cos(theta), Math.Sin(phi) * Math.Cos(theta), Math.Sin(theta));
         }
     }
 }
--- src/AirplaneSimulationTrajectory/AirplaneSimulationTrajectory/ViewModel/MainViewModel.cs
+++ src/AirplaneSimulationTrajectory/AirplaneSimulationTrajectory/ViewModel/MainViewModel.cs
@@ -1,26 +1,71 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Timers;
-using System.Windows;
+using System.Windows.Media;
 using System.Windows.Media.Media3D;
-using AirplaneSimulationTrajectory.View;
+using AirplaneSimulationTrajectory.Services;
 using HelixToolkit.Wpf;
 
 namespace AirplaneSimulationTrajectory.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+        private FileModelVisual3D _aircraft;
+
+        private HelixViewport3D _cameraView3D;
+
+        private Material _clouds;
+
+        private HelixViewport3D _mainViewport3D;
+
+        private Vector3D _sunlightDirection;
+        private readonly AircraftService aircraftService;
         private Timer timer;
 
         public MainViewModel()
         {
+            //TODO USE the DI.
+            aircraftService = new AircraftService();
             Clouds = MaterialHelper.CreateImageMaterial("pack://application:,,,/Images/clouds.jpg", 0.5);
             var now = DateTime.UtcNow;
             var juneSolstice = new DateTime(now.Year, 6, 22);
+
+            Calculation(now, juneSolstice);
+
+            SetAircraftPath();
+
+            InitializeTimer();
         }
 
-        private Material _clouds;
+        public HelixViewport3D CameraView3D
+        {
+            get => _cameraView3D;
+            set
+            {
+                _cameraView3D = value;
+                SetField(ref _cameraView3D, value, nameof(CameraView3D));
+            }
+        }
 
-        private Vector3D _sunlightDirection;
+        public HelixViewport3D MainViewport3D
+        {
+            get => _mainViewport3D;
+            set
+            {
+                _mainViewport3D = value;
+                SetField(ref _mainViewport3D, value, nameof(MainViewport3D));
+            }
+        }
+
+        public FileModelVisual3D Aircraft
+        {
+            get => _aircraft;
+            set
+            {
+                _aircraft = value;
+                SetField(ref _aircraft, value, nameof(Aircraft));
+            }
+        }
 
         public Material Clouds
         {
@@ -41,6 +86,42 @@ namespace AirplaneSimulationTrajectory.ViewModel
                 _sunlightDirection = value;
                 SetField(ref _sunlightDirection, value, nameof(SunlightDirection));
             }
+        }
+
+        private void SetAircraftPath()
+        {
+            //set start and finish pos of plane
+            var from = new Vector3D(1, 0, 0);
+            var to = new Vector3D(0, 1, 1);
+            aircraftService.SetPlanePath(from, to);
+        }
+
+        private void InitializeTimer()
+        {
+            //create timer for updating
+            timer = new Timer(30);
+            timer.AutoReset = true;
+            timer.Elapsed += (o, e) => { Task.Run(() => OnTimerTick(null, null)); };
+            timer.Start();
+        }
+
+        private void Calculation(DateTime now, DateTime juneSolstice)
+        {
+            SunlightDirection = aircraftService.CalculateMove(now, juneSolstice);
+            CameraView3D.Camera.LookDirection = SunlightDirection;
+            CameraView3D.Camera.Position = new Point3D() - CameraView3D.Camera.LookDirection;
+            CameraView3D.Title = now.ToString("u");
+            CameraView3D.TextBrush = Brushes.White;
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            var result = aircraftService.UpdateAircraftPosition();
+            Aircraft.Transform = result.planeTransform;
+            MainViewport3D.InvalidateVisual();
+
+            //new Plane position
+            aircraftService.AircraftPosition = result.secondPosition;
         }
     }
 }
