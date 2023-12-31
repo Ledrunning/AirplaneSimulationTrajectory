dffbe22c131e41bea92cbbb0497b98f74f00a136
Ledrunning <ledrunner@hotmail.com>
2023-12-31 22:53:47 +0100

    Added flight information

---
+++ src/AirplaneSimulationTrajectory/AirplaneSimulationTrajectory/View/Controls/FlightInfo/FlightInfoControlDataContext.cs
@@ -0,0 +1,45 @@
+﻿using AirplaneSimulationTrajectory.ViewModel;
+
+namespace AirplaneSimulationTrajectory.View.Controls.FlightInfo
+{
+    public class FlightInfoControlDataContext : BaseViewModel
+    {
+        private string _title;
+        private string _coordinates;
+        private string _currentTime;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public string CurrentTime
+        {
+            get => _currentTime;
+            set
+            {
+                _currentTime = value;
+                OnPropertyChanged(nameof(CurrentTime));
+            }
+        }
+
+        public string TotalFlightTime { get; set; }
+        public string FlightLength { get; set; }
+        public string Temperature { get; set; }
+
+        public string Coordinates
+        {
+            get => _coordinates;
+            set
+            {
+                _coordinates = value;
+                OnPropertyChanged(nameof(Coordinates));
+            }
+        }
+    }
+}
