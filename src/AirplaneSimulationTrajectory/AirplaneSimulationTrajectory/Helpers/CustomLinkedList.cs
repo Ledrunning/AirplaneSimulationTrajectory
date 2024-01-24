30efa241ec09336d39e7fd45f141cb6ec9ecfef4
Ledrunning <ledrunner@hotmail.com>
2024-01-24 23:31:29 +0100

    Refactored CustomLinkedList

--- src/AirplaneSimulationTrajectory/AirplaneSimulationTrajectory/Helpers/CustomLinkedList.cs
+++ src/AirplaneSimulationTrajectory/AirplaneSimulationTrajectory/Helpers/CustomLinkedList.cs
@@ -4,16 +4,28 @@ namespace AirplaneSimulationTrajectory.Helpers
 {
     public class CustomLinkedList<T> : List<T>
     {
-        public T Current => this[_currentIndex];
+        private int _currentIndex;
 
-        private int _currentIndex = 0;
+        public CustomLinkedList(IEnumerable<T> collection) : base(collection)
+        {
+        }
+
+        public CustomLinkedList(params T[] paramList) : base(paramList)
+        {
+        }
+
+        public T Current => this[_currentIndex];
 
         public T Next
         {
             get
             {
-                if (this.Count == 0) return default(T);
-                if (_currentIndex < this.Count - 1)
+                if (Count == 0)
+                {
+                    return default;
+                }
+
+                if (_currentIndex < Count - 1)
                 {
                     _currentIndex++;
                 }
@@ -26,18 +38,10 @@ namespace AirplaneSimulationTrajectory.Helpers
             }
         }
 
-        public CustomLinkedList(IEnumerable<T> collection) : base(collection)
-        {
-        }
-
-        public CustomLinkedList(params T[] paramList) : base(paramList)
-        {
-        }
-
         public void AddList(params T[] paramList)
         {
-            base.Clear();
-            base.AddRange(paramList);
+            Clear();
+            AddRange(paramList);
         }
     }
 }
